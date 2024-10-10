75747069214b6b73432545e6c8a0f809bd3b3853
Parker <devhubmain@gmail.com>
2024-10-11 02:52:16 +0500

    (update) conform plugin config

---
+++ lua/neovim/configs/conform.lua
@@ -0,0 +1,34 @@
+-- Установка плагина с помощью менеджера плагинов
+local conform = require "conform"
+
+conform.setup {
+	formatters_by_ft = {
+		javascript = { "prettier" },
+		typescript = { "prettier" },
+		javascriptreact = { "prettier" },
+		typescriptreact = { "prettier" },
+		svelte = { "prettier" },
+		css = { "prettier" },
+		html = { "prettier" },
+		json = { "prettier" },
+		yaml = { "prettier" },
+		markdown = { "prettier" },
+		graphql = { "prettier" },
+		liquid = { "prettier" },
+		lua = { "stylua" },
+		python = { "isort", "black" },
+	},
+	format_on_save = {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 1000,
+	},
+}
+-- Пример использования команды для форматирования файла
+vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+	conform.format {
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 1000,
+	}
+end, { desc = "Format file or range (in visual mode)" })
