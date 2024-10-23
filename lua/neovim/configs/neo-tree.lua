98ddb68c3e1a9557f1627033236a449acc1d5d1e
Parker <devhubmain@gmail.com>
2024-10-24 01:22:50 +0500

    (update) changed git icons to letters, add search and filter on current directory

--- lua/neovim/configs/neo-tree.lua
+++ lua/neovim/configs/neo-tree.lua
@@ -2,24 +2,41 @@ require("neo-tree").setup {
 	close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
 	enable_git_status = true,
 	enable_diagnostics = true,
-}
-default_component_configs = {
-	container = {
-		enable_character_fade = true,
-	},
-	git_status = {
-		symbols = {
-			-- Change type
-			added = "✚", -- or "✚", but this is redundant info if you use git_status_colors on the name
-			modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
-			deleted = "✖", -- this can only be used in the git_status source
-			renamed = "󰁕", -- this can only be used in the git_status source
-			-- Status type
-			untracked = "",
-			ignored = "",
-			unstaged = "󰄱",
-			staged = "",
-			conflict = "",
+	default_component_configs = {
+		container = {
+			enable_character_fade = true,
 		},
+		git_status = {
+			symbols = {
+				-- Change type
+				added = "A", -- ✚ -- or "✚", but this is redundant info if you use git_status_colors on the name
+				modified = "M", --  -- or "", but this is redundant info if you use git_status_colors on the name
+				deleted = "D", -- ✖ -- this can only be used in the git_status source
+				renamed = "R", -- 󰁕 -- this can only be used in the git_status source
+				-- Status type
+				untracked = "UT", -- 
+				ignored = "I", -- 
+				unstaged = "US", -- 󰄱
+				staged = "S", -- 
+				conflict = "C", -- 
+			},
+		},
+	},
+
+	filesystem = {
+		follow_current_file = { -- обновлять дерево на основе текущего файла
+			enabled = true,
+		},
+		use_libuv_file_watcher = true, -- ускорение обновления файлов
+		hijack_netrw_behavior = "open_default",
+
+		-- filtered_items - возможно причина торможения плагина
+
+		filtered_items = {
+			visible = false, -- показать скрытые файлы
+			hide_dotfiles = true,
+			hide_gitignored = true,
+		},
+		find_by = "cwd", -- ограничить поиск текущей директорией
 	},
 }
